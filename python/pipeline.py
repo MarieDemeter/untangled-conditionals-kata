e9524df2288ec22b516e708b6a6b9685e4d23bc1
MarieDemeter <mdemeter.ducrot@gmail.com>
2022-10-21 15:38:20 +0200

    Refactoring with exception to delete all boolean

--- python/pipeline.py
+++ python/pipeline.py
@@ -1,3 +1,11 @@
+class TestFailedException(Exception):
+    pass
+
+
+class DeploymentFailedException(Exception):
+    pass
+
+
 class Pipeline:
     def __init__(self, config, emailer, log):
         self.config = config
@@ -5,10 +13,14 @@ class Pipeline:
         self.log = log
 
     def run(self, project):
-        tests_passed = self.run_tests(project)
-        deploy_successful = self.deploy_successful(project, tests_passed)
-        summary = self.create_email_summary(deploy_successful, tests_passed)
-        self.send_email(summary)
+        try:
+            self.run_tests(project)
+            self.deploy_successful(project)
+            self.send_email("Deployment completed successfully")
+        except TestFailedException:
+            self.send_email("Tests failed")
+        except DeploymentFailedException:
+            self.send_email("Deployment failed")
 
     def send_email(self, summary):
         if not self.config.send_email_summary():
@@ -18,34 +30,20 @@ class Pipeline:
         self.log.info("Sending email")
         self.emailer.send(summary)
 
-    def create_email_summary(self, deploy_successful, tests_passed):
-        if not tests_passed:
-            return "Tests failed"
-
-        if not deploy_successful:
-            return "Deployment failed"
-
-        return "Deployment completed successfully"
-
-    def deploy_successful(self, project, tests_passed):
-        if not tests_passed:
-            return False
-
+    def deploy_successful(self, project):
         if "success" != project.deploy():
             self.log.error("Deployment failed")
-            return False
+            raise DeploymentFailedException
 
         self.log.info("Deployment successful")
-        return True
 
     def run_tests(self, project):
         if not project.has_tests():
             self.log.info("No tests")
-            return True
+            return
 
         if "success" != project.run_tests():
             self.log.error("Tests failed")
-            return False
+            raise TestFailedException
 
         self.log.info("Tests passed")
-        return True
