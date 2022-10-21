ff74b914e6a2ab1a4484088bc5e4af636b2627aa
MarieDemeter <mdemeter.ducrot@gmail.com>
2022-10-21 11:31:38 +0200

    Refactoring python pipeline.py

--- python/pipeline.py
+++ python/pipeline.py
@@ -5,38 +5,51 @@ class Pipeline:
         self.log = log
 
     def run(self, project):
-        tests_passed = False
-        deploy_successful = False
+        tests_passed = self.run_tests(project)
+        deploy_successful = self.deploy_successful(project, tests_passed)
+        summary = self.create_email_summary(deploy_successful, tests_passed)
+        self.send_email(summary)
 
-        if project.has_tests():
-            if "success" == project.run_tests():
-                self.log.info("Tests passed")
-                tests_passed = True
+    def send_email(self, summary):
+        if self.config.send_email_summary():
+            self.log.info("Sending email")
+            self.emailer.send(summary)
+        else:
+            self.log.info("Email disabled")
+
+    def create_email_summary(self, deploy_successful, tests_passed):
+        if tests_passed:
+            if deploy_successful:
+                summary = "Deployment completed successfully"
             else:
-                self.log.error("Tests failed")
-                tests_passed = False
+                summary = "Deployment failed"
+        else:
+            summary = "Tests failed"
+        return summary
+
+    def deploy_successful(self, project, tests_passed):
+        if tests_passed:
+            deploy_success = self.is_success(project.deploy(), "Deployment successful", "Deployment failed")
+        else:
+            deploy_success = False
+
+        return deploy_success
+
+    def run_tests(self, project):
+        if project.has_tests():
+            tests_passed = self.is_success(project.run_tests(), "Tests passed", "Tests failed")
         else:
             self.log.info("No tests")
             tests_passed = True
 
-        if tests_passed:
-            if "success" == project.deploy():
-                self.log.info("Deployment successful")
-                deploy_successful = True
-            else:
-                self.log.error("Deployment failed")
-                deploy_successful = False
-        else:
-            deploy_successful = False
+        return tests_passed
 
-        if self.config.send_email_summary():
-            self.log.info("Sending email")
-            if tests_passed:
-                if deploy_successful:
-                    self.emailer.send("Deployment completed successfully")
-                else:
-                    self.emailer.send("Deployment failed")
-            else:
-                self.emailer.send("Tests failed")
+    def is_success(self, method, log_info, log_error):
+        if "success" == method:
+            self.log.info(log_info)
+            result = True
         else:
-            self.log.info("Email disabled")
+            self.log.error(log_error)
+            result = False
+
+        return result
